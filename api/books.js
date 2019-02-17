eee31471426c211872982ce3faf677ebcec1a54c
Bajju <bharadwajsampath@live.com>
2019-02-17 21:19:33 +1100

    adding eslint

--- api/books.js
+++ api/books.js
@@ -1,26 +1,23 @@
 import firebase from 'firebase';
 
-const postNewBook = (bookDetails) =>{
-    
-    var newPostKey = firebase.database().ref().child('books').push().key;
-    firebase.database().ref('books/'+ newPostKey).set({
-      author: bookDetails.author,
-      available: bookDetails.available,
-      price: bookDetails.price,
-      title: bookDetails.title
-    })
-
-}
+const postNewBook = (bookDetails) => {
+  const newPostKey = firebase.database().ref().child('books').push().key;
+  firebase.database().ref(`books/${newPostKey}`).set({
+    author: bookDetails.author,
+    available: bookDetails.available,
+    price: bookDetails.price,
+    title: bookDetails.title
+  });
+};
 
 
-const getAllBooks = () =>{
-    
-    firebase.database().ref('books/').once('value').then((snapshot) =>{
-        console.log(snapshot);
-    })
-}
+const getAllBooks = () => {
+  firebase.database().ref('books/').once('value').then((snapshot) => {
+    console.log(snapshot);
+  });
+};
 
 export default {
-    post: postNewBook,
-    all: getAllBooks,
-}
+  post: postNewBook,
+  all: getAllBooks,
+};
--- screens/AddBookScreen.js
+++ screens/AddBookScreen.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
 
 export default class LinksScreen extends React.Component {
   static navigationOptions = {
-    title: 'Links',
+    title: 'Add a Book',
   };
 
   render() {
     return (
-      <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
-      </ScrollView>
+      <ScrollView style={styles.container} />
     );
   }
 }
