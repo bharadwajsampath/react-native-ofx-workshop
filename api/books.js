7cbf3271df4adc0820f3b2552136c84c0b1fe909
Bajju <bharadwajsampath@live.com>
2019-02-17 20:45:24 +1100

    Basic setup

---
+++ api/books.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase';
+
+const postNewBook = (bookDetails) =>{
+    
+    var newPostKey = firebase.database().ref().child('books').push().key;
+    firebase.database().ref('books/'+ newPostKey).set({
+      author: bookDetails.author,
+      available: bookDetails.available,
+      price: bookDetails.price,
+      title: bookDetails.title
+    })
+
+}
+
+
+const getAllBooks = () =>{
+    
+    firebase.database().ref('books/').once('value').then((snapshot) =>{
+        console.log(snapshot);
+    })
+}
+
+export default {
+    post: postNewBook,
+    all: getAllBooks,
+}
---
+++ navigation/MainTabNavigator.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+
+import TabBarIcon from '../components/TabBarIcon';
+import HomeScreen from '../screens/HomeScreen';
+import LinksScreen from '../screens/LinksScreen';
+import SettingsScreen from '../screens/SettingsScreen';
+
+const HomeStack = createStackNavigator({
+  Home: HomeScreen,
+});
+
+HomeStack.navigationOptions = {
+  tabBarLabel: 'All Books',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={
+        Platform.OS === 'ios'
+          ? 'ios-book'
+          : 'md-book'
+      }
+    />
+  ),
+};
+
+const LinksStack = createStackNavigator({
+  Links: LinksScreen,
+});
+
+LinksStack.navigationOptions = {
+  tabBarLabel: 'Add a Book',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-add' : 'md-add'}
+    />
+  ),
+};
+
+const SettingsStack = createStackNavigator({
+  Settings: SettingsScreen,
+});
+
+SettingsStack.navigationOptions = {
+  tabBarLabel: 'Settings',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'}
+    />
+  ),
+};
+
+export default createBottomTabNavigator({
+  HomeStack,
+  LinksStack,
+  SettingsStack,
+});
